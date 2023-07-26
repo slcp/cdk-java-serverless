ff9f1b4b448780a42d286ae95420ace714ca0559
Stuart Forrest <stuart.forrest@infinityworks.com>
2023-07-26 17:32:06 +0100

    Add a lambda layer for dynamo access

--- infra/src/main/java/com/myorg/JavaCdkStack.java
+++ infra/src/main/java/com/myorg/JavaCdkStack.java
@@ -2,6 +2,7 @@ package com.myorg;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.RemovalPolicy;
@@ -15,43 +16,53 @@ import software.amazon.awscdk.services.dynamodb.Table;
 import software.amazon.awscdk.services.lambda.Code;
 import software.amazon.awscdk.services.lambda.Function;
 import software.amazon.awscdk.services.lambda.FunctionProps;
+import software.amazon.awscdk.services.lambda.LayerVersion;
+import software.amazon.awscdk.services.lambda.LayerVersionProps;
 import software.amazon.awscdk.services.lambda.Runtime;
 import software.amazon.awscdk.services.logs.RetentionDays;
 import software.constructs.Construct;
 
 public class JavaCdkStack extends Stack {
-    public JavaCdkStack(final Construct scope, final String id) {
-        this(scope, id, null);
-    }
+        public JavaCdkStack(final Construct scope, final String id) {
+                this(scope, id, null);
+        }
 
-    public JavaCdkStack(final Construct scope, final String id, final StackProps props) {
-        super(scope, id, props);
+        public JavaCdkStack(final Construct scope, final String id, final StackProps props) {
+                super(scope, id, props);
 
-        String CDK_DIR = System.getProperty("user.dir");
-        String APPLICATION_DIR = "../lambda-application";
-        Path path = Paths.get(CDK_DIR, APPLICATION_DIR,
-                "/lambda-1/target/lambda-1-1.0-SNAPSHOT.jar");
+                String CDK_DIR = System.getProperty("user.dir");
+                String APPLICATION_DIR = "../lambda-application";
 
-        Function functionOne = new Function(this, "FunctionOne", FunctionProps.builder()
-                .runtime(Runtime.JAVA_17)
-                .code(Code.fromAsset(path.toAbsolutePath().toString()))
-                .handler("com.tericcabrel.App")
-                .memorySize(256)
-                .timeout(Duration.seconds(30))
-                .logRetention(RetentionDays.ONE_WEEK)
-                .build());
+                Path layerPath = Paths.get(CDK_DIR, APPLICATION_DIR,
+                                "/lambda-layer/target/out");
+                LayerVersion layer = new LayerVersion(this, "layer", LayerVersionProps.builder()
+                                .code(Code.fromAsset(layerPath.toAbsolutePath().toString()))
+                                .compatibleRuntimes(Arrays.asList(Runtime.JAVA_17))
+                                .build());
 
-        LambdaRestApi.Builder.create(this, "myapi")
-                .handler(functionOne)
-                .build();
+                Path functionOnePath = Paths.get(CDK_DIR, APPLICATION_DIR,
+                                "/lambda-1/target/lambda-1-1.0-SNAPSHOT.jar");
+                Function functionOne = new Function(this, "FunctionOne", FunctionProps.builder()
+                                .runtime(Runtime.JAVA_17)
+                                .code(Code.fromAsset(functionOnePath.toAbsolutePath().toString()))
+                                .handler("com.tericcabrel.App")
+                                .memorySize(256)
+                                .timeout(Duration.seconds(30))
+                                .layers(Arrays.asList(layer))
+                                .logRetention(RetentionDays.ONE_WEEK)
+                                .build());
 
-        Table table = Table.Builder.create(this, "Table")
-                .partitionKey(Attribute.builder().name("id").type(AttributeType.STRING).build())
-                .billingMode(BillingMode.PAY_PER_REQUEST)
-                .removalPolicy(RemovalPolicy.DESTROY)
-                .build();
+                LambdaRestApi.Builder.create(this, "myapi")
+                                .handler(functionOne)
+                                .build();
 
-        functionOne.addEnvironment("TABLE_NAME", table.getTableName());
-        table.grantReadWriteData(functionOne);
-    }
+                Table table = Table.Builder.create(this, "Table")
+                                .partitionKey(Attribute.builder().name("id").type(AttributeType.STRING).build())
+                                .billingMode(BillingMode.PAY_PER_REQUEST)
+                                .removalPolicy(RemovalPolicy.DESTROY)
+                                .build();
+
+                functionOne.addEnvironment("TABLE_NAME", table.getTableName());
+                table.grantReadWriteData(functionOne);
+        }
 }
